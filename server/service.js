9423b30782e5b0046d2c70712a09ebef33f79bdc
Adrian Darian <adarian@ucmerced.edu>
2019-01-31 23:50:18 -0800

    ehhhh

---
+++ server/service.js
@@ -0,0 +1,56 @@
+// Require the serialport node module
+var serialport = require('serialport');
+var fs = require('fs');
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic('./index.html')).listen(8080, function () {
+  console.log('Server running on 8080...');
+});
+
+// Open the port
+var port = new serialport("/dev/cu.HC-06-DevB", {
+  baudRate: 9600 // remember to check baudrate
+});
+
+let points = {};
+let counter = 0;
+
+// Read the port data
+port.on("open", function () {
+  console.log('open\n');
+  let key = "test" + counter.toString();
+  points[key] = {};
+  port.on('data', function (data) {
+    let val = 0;
+    let result = [];
+    for (var i = 0; i < data.length; i++) {
+      result[i] = data[i];
+    }
+    points[key][val] = result;
+    val++;
+    // console.log('\033[2J'); // clear console
+    console.log(result, ":len:", data.length);
+    file = fs.createWriteStream('./test.txt');
+    var buf = Buffer.from(JSON.stringify(points));
+    file.write(buf);
+
+    function end() {
+      file.end();
+      console.log("Stream closed.")
+      return 0;
+    }
+
+    const sleep = (ms) => {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    const test = async () => {
+      await sleep(5000);
+      file.end();
+    }
+
+    test();
+  });
+  key++;
+});
